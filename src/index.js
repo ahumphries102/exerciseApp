ee59fe319259305b250cc8d7bb633da747a1b2e1
AJ <ahumphries102@gmail.com>
2019-08-14 21:49:41 -0500

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Navbar from './Components/Navbar.component.js'
 import './index.css';
 ReactDOM.render(
-  <App />,
+    <Navbar/>,
   document.getElementById('root')
 );
